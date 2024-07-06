26c11af8cab68337ed29b9e407596944229a62f5
SHASHWAT  GARG <gargshashwat705@gmail.com>
2024-07-06 11:25:36 +0530

    last commit

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import blogRouter from "./routes/blogRouter.js";
+import fileUpload from "express-fileupload";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200, // for older browsers
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Test CORS route
+app.get("/test-cors", (req, res) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.send("CORS headers set!");
+});
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/blog", blogRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
